d98b6bf722bf7c8b6fe4488cb9b9d9d6f37ef2dc
Kelvin Okornoe <kelvin.okornoe@gmail.com>
2020-07-05 20:11:41 +0000

    initial commit

---
+++ Coffee Machine/task/src/machine/Main.kt
@@ -0,0 +1,66 @@
+package machine
+
+import java.util.*
+
+fun main() {
+
+    /**
+     * If the coffee machine has enough supplies to make the specified amount of coffee,
+     * the program should print "Yes, I can make that amount of coffee".
+     * If the coffee machine can make more than that,
+     * the program should output "Yes, I can make that amount of coffee (and even N more than that)",
+     * where N is the number of additional cups of coffee that the coffee machine can make.
+     * If the amount of resources is not enough to make the specified amount of coffee,
+     * the program should output "No, I can make only N cups of coffee".
+     */
+
+    val  sc = Scanner(System.`in`)
+
+    println("Write how many ml of water the coffee machine has: ")
+    val litersOfWater = sc.nextInt()
+
+    println("Write how many ml of milk the coffee machine has: ")
+    val litersOfMilk = sc.nextInt()
+
+    println("Write how many grams of coffee beans the coffee machine has: ")
+    val gramsOfCoffee = sc.nextInt()
+
+    println("Write how many cups of coffee you will need: ")
+    val cupsOfCoffee = sc.nextInt()
+
+
+    calculateAmountOfCoffee(litersOfWater,  litersOfMilk, gramsOfCoffee, cupsOfCoffee)
+}
+
+fun calculateAmountOfCoffee(water : Int, milk: Int, coffeeBeans: Int, cupsOfCoffee: Int){
+    // if coffee machine has enough supplies, "yes I can make that amount of coffee"
+
+    if (water ==  (200 * cupsOfCoffee) && milk == (50 * cupsOfCoffee) && coffeeBeans == (15 * cupsOfCoffee) ) {
+        println("Yes, I can make that amount of coffee")
+    }
+
+    // if the amount of resources requires is not enough to make the specified amount
+    // "No, I can make only N cups of coffee"
+
+    else if ((water < (200 * cupsOfCoffee)) || (milk < (50 * cupsOfCoffee))
+            || (coffeeBeans < (15 * cupsOfCoffee))) {
+
+        if (water < (200 * cupsOfCoffee)) {
+            println("No, I can make only ${water/(200)} cup(s) of coffee")
+        }
+    }
+
+    // if coffee machine machine can make that amount and more,
+    // "yes I can make that amount of coffee and n more"
+
+    if (water >  (200 * cupsOfCoffee) && milk > (50 * cupsOfCoffee) && coffeeBeans > (15 * cupsOfCoffee) ) {
+        if (cupsOfCoffee > 0) {
+            println("Yes, I can make that amount of coffee (and even ${water / (200 * cupsOfCoffee)} more than that)")
+        } else {
+            println("Yes, I can make that amount of coffee (and even 1 more than that)")
+        }
+    }
+}
+
+
+
